da3cf2ed286f6db59f21cc612f3686a089b3f53d
yuuuuna0 <jyn9306@gmail.com>
2022-12-02 18:15:36 +0900

    ..

--- 19.컬렉션[Collection]/src/generic/Test.java
+++ 19.컬렉션[Collection]/src/generic/Test.java
@@ -3,8 +3,10 @@ package generic;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("sdsdsd");
+		System.out.println("sdsdsd");
+		System.out.println("sdsdsd");
+		System.out.println("sdsdsd");
 	}
 
 }

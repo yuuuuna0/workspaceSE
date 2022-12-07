5d460348f8bdcdf94efe08cb2fddff655b5424d3
yuuuuna0 <jyn9306@gmail.com>
2022-12-07 14:04:42 +0900

    ..

--- 21.스레드[Thread]/src/ControlStopThread2.java
+++ 21.스레드[Thread]/src/ControlStopThread2.java
@@ -1,5 +1,36 @@
 
 public class ControlStopThread2 extends Thread{
+	private boolean isPlay;				
+	public ControlStopThread2() {
+		isPlay=true;
+	}
 	
-	
+
+	public void setPlay(boolean isPlay) {
+		this.isPlay = isPlay;
+	}
+
+
+	@Override
+	public void run() {
+		while(isPlay) {			//isPlay가 false로 지정되어도 5번까지 출력 후 조건문을 빠져나오게 된다.
+			try {
+				System.out.println("---------------------");
+				System.out.println("1. game move!");
+				Thread.sleep((int)(Math.random()*1000)+1);
+				System.out.println("2. game fight!");
+				Thread.sleep((int)(Math.random()*1000)+1);
+				System.out.println("3. game get score!");
+				Thread.sleep((int)(Math.random()*1000)+1);
+				System.out.println("4. game power up!");
+				Thread.sleep((int)(Math.random()*1000)+1);
+				System.out.println("5. game score update!");
+				Thread.sleep((int)(Math.random()*1000)+1);
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return;
+		//end run
+	}
 }
--- 21.스레드[Thread]/src/ControlStopThreadMain.java
+++ 21.스레드[Thread]/src/ControlStopThreadMain.java
@@ -1,14 +1,20 @@
 
 public class ControlStopThreadMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("1. main");
+		/*
 		ControlStopThread1 cst1=new ControlStopThread1();
 		cst1.start();
-		
-		
-		
-		
+		Thread.sleep(1000);
+		//cst1.stop();
+		*/
+		ControlStopThread2 cst2=new ControlStopThread2();
+		cst2.start();
+		Thread.sleep(5000);
+		//cst2.stop();
+		cst2.setPlay(false);
+		Thread.sleep(100000);
 		System.out.println("9. main thread jvm return");
 	}
 

bc55f4630d2115bf01bb55573d033adc3cd1c443
yuuuuna0 <jyn9306@gmail.com>
2022-12-07 16:18:48 +0900

    ..

--- 21.스레드[Thread]/src/SynchronizeUpperLowerThreadMain.java
+++ 21.스레드[Thread]/src/SynchronizeUpperLowerThreadMain.java
@@ -1,5 +1,6 @@
 /*
- * 동기화
+ * 동기화 제한자 synchronized
+ * 동기화를 사용할 떄는 동기화 된 객체는 한개만 생성해야 한다(열쇠는 하나만 생성되어야 함)
  */
 public class SynchronizeUpperLowerThreadMain {
 
@@ -9,8 +10,6 @@ public class SynchronizeUpperLowerThreadMain {
 		SynchronizeLowerThread lowerThread=new SynchronizeLowerThread(sharedObject);
 		upperThread.start();
 		lowerThread.start();
-		
-		
 	}
 
 }

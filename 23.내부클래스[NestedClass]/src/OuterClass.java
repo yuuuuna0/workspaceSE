a3432b34c28905623ade1cb8a1f3a5cbb4d0084b
yuuuuna0 <jyn9306@gmail.com>
2022-12-09 11:11:52 +0900

    ..

--- 23.내부클래스[NestedClass]/src/OuterClass.java
+++ 23.내부클래스[NestedClass]/src/OuterClass.java
@@ -6,8 +6,12 @@ public class OuterClass {
 	public void outer_member_method() {
 		System.out.println("OuterClass.member_method()");
 	}
-	/***********************************************************/
-	
+	/**************Outter(외부클래스)에서 Inner(내부클래스)의 객체 생성 후 사용***********/
+	public void outer_inner_class_use() {
+		InnerClass ic=new InnerClass();		//OuterClass.InnerClass ic=new OuterClass.InnerClass();
+		System.out.println(ic.inner_member_field=12121212);
+		ic.inner_member_method();
+	}
 	
 	
 	
@@ -27,7 +31,10 @@ public class OuterClass {
 		/**************Inner(내부클래스)에서 Outer(외부클래스)의 멤버로 접근***********/
 		public void inner_outer_member_access() {
 			System.out.println(InnerClass.class);
+			System.out.println(this);	//InnerClass 객체참조
 			System.out.println(OuterClass.this);
+			outer_member_field=9999;	//외부클래스 참조 없어도 사용할 수 있다. 호출 한 멤버는 InnerClass에서 먼저 찾은 후 OuterClass에서 찾아본다.
+			outer_member_method();
 		}
 	}
 	

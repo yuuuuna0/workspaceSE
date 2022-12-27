c3f6b168b478fa5be4d958e84628c7283675dccb
yuuuuna0 <jyn9306@gmail.com>
2022-12-27 15:09:55 +0900

    ..

--- 25.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/AddressDao4TestMain.java
+++ 25.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/AddressDao4TestMain.java
@@ -1,25 +1,25 @@
-package dao.address.third;
+package dao.address.fourth;
 
-public class AddressDao3TestMain {
+public class AddressDao4TestMain {
 
 	public static void main(String[] args) throws Exception{
 		/*
-		 * AddressDao3TestMain	에서
-		 * AddressDao3로 파라메타 객체(DTO,VO)를 전달
+		 * AddressDao4TestMain	에서
+		 * AddressDao4Main으로 데이터 리턴을 전달
 		 */
-		AddressDao3 addressDao3=new AddressDao3();
+		AddressDao4 addressDao4=new AddressDao4();
 		System.out.println("1. insert");
 		Address newAddress= new Address(0,"홍진경","555-5555","대구시민");
-		addressDao3.insert(newAddress);
+		addressDao4.insert(newAddress);
 		System.out.println("2. update");
 		Address updateAddress=new Address(10,"김변경","000-0000","주소변경");
-		addressDao3.update(updateAddress);
+		addressDao4.update(updateAddress);
 		System.out.println("3. delete");
-		addressDao3.delete(20);
+		addressDao4.delete(20);
 		System.out.println("4. findByPrimaryKey");
-		addressDao3.findByPrimaryKey(10);
+		addressDao4.findByPrimaryKey(10);
 		System.out.println("5. findAll");
-		addressDao3.findAll();
+		addressDao4.findAll();
 	}
 
 }
